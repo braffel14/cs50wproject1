f07acb39babac1137d01429e9197788b9c8ef55a
Benjamin Raffel <braffel@wisc.edu>
2020-06-16 12:03:07 -0500

    Finished up and ready to turn in
    Support added for creating reviews and basic api support added. Readme.md is also filled out with a description and information on the project.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cs50wproject1
 Project 1 - Books for Harvard CS50W
+
+Video screencast demonstration: https://youtu.be/rHf5J9lrO3U
+
+For this project I was instructed to create a basic book review website similar to goodReads. Once you reach the home screen, you are prompted to log in. From the login page you can login if you have a username or password or you can navigate to the registration page to create an account. The passwords are stored as encrypted text in the database and are not just plain text. After logging in, you are able to search books by isbn, title, or author. You can select the book you're searching for and view its previous reviews as well as it's average score pulled form goodReads' api. There is a button to create a new review which you can rate the book and leave a message. Once posted, your review will immediately appear on the book's page for people to see.
+
+The site also has a basici api functionality as well. If you navigate to /api/<isbn> it will return a json response with information on the book from the requested isbn. 
+
+The backend of the wbsite is all run through Flask and code is located inteh application.py file. This is where all the postgreSQL communication with the externally hosted database (hosted by Heroku) happens as well. All of the html files are located in the templates folder. base.html contains the main template for each page and then all of the other html files inherit the base code through flask's jinja2 integration. I used bootstrap for a lot of the CSS work but there is also custom CSS contained in the /static/styles directory. It is written as SASS in styles.scss and then rendered to CSS instyles.css
--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, redirect, url_for, request, abort
+from flask import Flask, session, render_template, redirect, url_for, request, abort, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -183,11 +183,92 @@ def book(isbn):
             'isbn': review['isbn'],
             'username': user['username'],
             'rating': review['rating'],
-            'review': review['review'],
+            'review': review['review'].replace("\'\'", "\'"),
         }
 
-    return render_template("book.html", info=info, reviews=reviews)
+    return render_template("book.html", info=info, reviews=reviews, cantreview=request.args.get("cantreview"), reviewsuccess=request.args.get("reviewsuccess"))
 
+@app.route("/newreview/<string:isbn>", methods=["GET", "POST"])
+def newreview(isbn):
+
+    #get title of the book
+    book = db.execute("SELECT * FROM BOOKS WHERE isbn = :isbn", {"isbn": isbn}).first()
+    if book is not None:
+        title = book['title']
+    else:
+        abort(404)
+
+    #get user_id of logged in user
+    user_id = db.execute("SELECT user_id FROM users WHERE username = :username", {"username":session["user"]}).first()
+    
+    #check if user already has published a review and redirect to book page if so
+    rdone = db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND user_id = :user_id ", {"isbn": isbn, "user_id":user_id[0]}).first()
+    if rdone:
+        return redirect(url_for('book', isbn=isbn, cantreview=True))
+
+    #get review info from form and publish review
+    if(request.method == "POST"):
+        rating = int(request.form.get("rating"))
+        reviewtext = request.form.get("reviewtext")
+        #fix single ' in review text to avoid sql upload issues
+        reviewtext = reviewtext.replace("\'", "\'\'")
+
+        db.execute("INSERT INTO reviews (isbn, user_id, rating, review) VALUES (:isbn, :user_id, :rating, :review)", {"isbn":str(isbn), "user_id":int(user_id[0]), "rating":int(rating), "review":str(reviewtext)})
+        db.commit()
+        
+        return redirect(url_for('book', isbn=isbn, reviewsuccess=True))
+
+
+    return render_template("newreview.html", isbn=isbn ,booktitle=title)
+
+@app.route("/api/<isbn>")
+def api(isbn):
+
+    #make sure book exists and gets info
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).first()
+    #return error if book does not exist
+    if book is None:
+        return jsonify({"error": "Invalid isbn"}), 404
+
+    #get rating info from goodreads
+    grjson = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": 'gbjpeSJgvTVrtvncXhNzdg', 'isbns':[isbn]})
+    grdata = grjson.json()
+
+    #get review count
+    dbreviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn", {"isbn": isbn}).fetchall()
+    reviewcount = 0
+    for review in dbreviews:
+        reviewcount = reviewcount + 1
+    print(reviewcount)
+
+
+
+    #organize info into dictionary
+    info = {
+    'isbn':isbn,
+    'title': book['title'],
+    'author': book['author'],
+    'year': book['year'],
+    'average_score': grdata['books'][0]['average_rating'],
+    'reviewcount': reviewcount
+    }
+
+    return jsonify({
+        "title": info['title'],
+        "author": info['author'],
+        "year": int(info['year']),
+        "isbn": info['isbn'],
+        "review_count": int(info['reviewcount']),
+        "average_score": float(info['average_score'])
+    })
+    
+
+
+    
+
+
+
+#404 error handler
 @app.errorhandler(404)
 def page_not_found(error):
    return render_template('404.html', title = '404'), 404
